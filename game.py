ebb90246b0381804c3a818fe536f007ea43b6c28
conniepocky <c.amer@icloud.com>
2019-04-16 21:53:14 +0100

    updated screen

--- game.py
+++ game.py
@@ -10,18 +10,16 @@ c = ["jjong", "key", "minho", "onew", "taemin"]
 minho  = Actor(choice(c))
 minho.topright = 0, 10
  
-WIDTH = 500
-HEIGHT = minho.height + 20
- 
+WIDTH = 1000
+HEIGHT = 500
  
 def draw():
     screen.clear()
     minho.draw()
- 
- 
+
 def update():
     minho.left += 2
     if minho.left > WIDTH:
         minho.right = 0
- 
+
 pgzrun.go()
